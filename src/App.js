82cefad1d0b1be119097d5ba9c3e47b7eb25f43c
spindyzel <40053870+spindyzel@users.noreply.github.com>
2019-08-05 11:07:26 +0700

    Basic javacript function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      date: new Date(),
+      name: 'Husni Ramdani',
+    };
+  }
+
+  componentDidMount() {
+    setInterval(
+      () => this.setState({
+          date: new Date(),
+        }),
+      1000
+    );
+  }
+
+  render(){
+    const { Header, Content, Footer } = Layout;
+    return (
+      <Layout className="layout" style={{ minHeight: "100vh"}}>
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">HOME</Menu.Item>
+            <Menu.Item key="2">STORE</Menu.Item>
+            <Menu.Item key="3">APPS</Menu.Item>
+          </Menu>
+        </Header>
+
+        <Content style={{ padding: '0 50px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+          <div style={{ background: '#fff', padding: 24, minHeight: "100%" }}>
+            <h1>Sekarang Jam</h1>
+            <h1>
+              {this.state.date.toLocaleTimeString()}
+              <br/>
+              {this.state.name}
+            </h1>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>HEHEHE 2019</Footer>
+        
+      </Layout>
+    );
+  }
 }
 
 export default App;
